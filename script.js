28a3d1c7ceacd0285d025fb4fe4adeb5f4e03f56
mustakrakishe <mustakrkish@gmail.com>
2022-03-23 10:42:45 +0200

    dinamic background.

--- script.js
+++ script.js
@@ -301,7 +301,7 @@ window.addEventListener('load', function(){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         background.draw(ctx);
-        // background.update();
+        background.update();
         player.draw(ctx);
         player.update(input, deltaTime, enemies);
         handleEnemies(deltaTime);
